c5066a4ab8873d8a1e1c94672cc8919e0856a348
TopVirusMinus <mwmma5000@gmail.com>
2023-01-14 20:59:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="header">
+        <h1>Learn code</h1>
+        <ul>
+          <li>
+            <a href="http://">home</a>
+          </li>
+          <li>
+            <a href="http://">faq</a>
+          </li>
+
+          <a href="http://">contact</a>
+        </ul>
+      </div>
+      <div className="main">conent here</div>
+      <div className="footer">footer</div>
+    </div>
+  );
+};
+
+export default App;
